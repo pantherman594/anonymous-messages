c9e87a8790b6bef608c3c9a5f14248349b72d2d3
David Shen <me@pantherman594.com>
2019-01-19 23:28:21 -0500

    Create app

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const axios = require('axios')
-const express = require('express')
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const express = require('express');
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-  res.json({
+  console.log(req.body);
+  res.status(200).send('');
+  axios.post(req.body.response_url, {
     response_type: "in_channel",
-    text: "ASDF",
+    text: req.body.text,
   });
 });
 
-app.listen(80, () => {
-  console.log("Listening on port 80...");
+app.get('/', (req, res) => {
+  res.send("HI!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
