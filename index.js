a3d08042b85a275d297219ebf5622de3c7b012f9
David Shen <me@pantherman594.com>
2019-01-20 01:07:53 -0500

    Remove token storage, not necessary

--- index.js
+++ index.js
@@ -5,24 +5,15 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const TOKENS_FILE = 'tokens.json';
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-let rawTokenData;
-try {
-  rawTokenData = fs.readFileSync(TOKENS_FILE);
-} catch(err) {
-  rawTokenData = '{}';
-}
-const authTokens = JSON.parse(rawTokenData);
 
 app.post('/', (req, res) => {
   console.log(req.body);
   res.status(200).send('');
   axios.post(req.body.response_url, {
-    response_type: "in_channel",
+    response_type: 'in_channel',
     text: req.body.text,
   });
 });
@@ -31,7 +22,7 @@ app.get('/oauth', (req, res) => {
   const code = req.query.code;
 
   if (!code) {
-    res.send("Installation has been cancelled.");
+    res.send('Installation has been cancelled.');
     return;
   }
 
@@ -41,18 +32,12 @@ app.get('/oauth', (req, res) => {
       client_secret: process.env.SLACK_SECRET,
       code: code,
     }
-  }).then(authRes => {
-    const token = authRes.access_token;
-    const id = authRes.team_id;
-
-    authTokens[id] = token;
-    fs.writeFileSync(TOKENS_FILE, JSON.stringify(authTokens));
   });
-  res.send("Anonymous Messages has been activated.");
+  res.send('Anonymous Messages has been activated.');
 });
 
 app.get('/', (req, res) => {
-  res.send("To install Anonymous Messages, go to https://slack.com/oauth/authorize?client_id=3291865129.529288519095&scope=commands");
+  res.send('To install Anonymous Messages, click <a href="https://slack.com/oauth/authorize?client_id=3291865129.529288519095&scope=commands">here</a>.');
 });
 
 app.listen(PORT, () => {
